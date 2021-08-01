bc9af05a531c47af343db833043b1cb24931f589
elik <elik@example.com>
2021-08-01 20:21:50 +0300

    Finish base framework functional

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import { Excel } from './components/excel/excel'
+import { Header } from './components/header/Header';
+import { Toolbar } from './components/toolbar/Toolbar';
+import { Formula } from './components/formula/Formula';
+import { Table } from './components/table/Table';
 import './scss/index.scss'
-// import 'core-js/stable'
-// import 'regenerator-runtime/runtime'
 
-console.log("Hello world!")
+const excel = new Excel('#app', {
+    components: [Header, Toolbar, Formula, Table]
+})
+
+excel.render()
